79cd2ca5814032e6996ae70184d521f2001af7cc
Daniel Boldan <boldan.daniel@gmail.com>
2017-02-03 00:26:56 +0000

    Add feature test

---
+++ tests/Feature/BlogTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class BlogTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBlogResponseIsValid()
+    {
+        $response = $this->get('/blog');
+        $response->assertStatus(200);
+    }
+}
